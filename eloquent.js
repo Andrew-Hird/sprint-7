a645c6d9b60e7f6b90c7aa76bb27b16d4fb929f8
Andrew Hird <andrew.james.hird@gmail.com>
2016-08-13 14:11:20 +1200

    Bean Counting Exercise

--- eloquent.js
+++ eloquent.js
@@ -1,4 +1,4 @@
-//Chapter Three http://eloquentjavascript.net/ Exercises
+//Chapter Three http://eloquentjavascript.net/03_functions.html Exercises
 
 //Minimum
 function min (x, y) {
@@ -25,3 +25,47 @@ function isEven(number) {
 console.log(isEven(50))
 console.log(isEven(75))
 console.log(isEven(-1))
+
+//Bean Counting
+
+//#1
+function countBs(str) {
+  var count = 0
+  for (var i = 0; i < str.length; i++) {
+    if (str.charAt(i) === 'B') {
+      count += 1
+ 	 }
+  }
+  return count
+}
+console.log(countBs("BBBBBBC"))
+
+//#2
+// Your code here.
+function countChar (str, char) {
+  var count = 0
+  for(var i = 0; i < str.length; i++) {
+    if(str.charAt(i) === char) {
+      count += 1
+    }
+  }
+  return count
+}
+console.log(countChar("kakkkkkerlak", "k"))
+
+//#3
+function countChar (str, char) {
+  var count = 0
+  for(var i = 0; i < str.length; i++) {
+    if(str.charAt(i) === char) {
+      count += 1
+    }
+  }
+  return count
+}
+
+function countBs(str) {
+  return countChar(str, 'B')
+}
+console.log(countBs("BBBBBBC"));
+console.log(countChar("kakkkkkerlak", "k"))
